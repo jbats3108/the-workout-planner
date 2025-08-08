932f44388a73edca38b21a8143949ca47a25163f
Jamie Batabyal <jamie.batabyal@tillo.io>
2025-08-08 15:06:14 +0100

    Seed roles and users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $admin = User::factory()->create([
+            'name'     => 'Admin',
+            'email'    => 'admin@admin.com',
+            'password' => bcrypt('password'),
+
+        ]);
+
+        $admin->assignRole(Role::findByName('admin'));;
+
+        $user1 = User::factory()->create([
+            'name'     => 'User 1',
+            'email'    => 'user1@test.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $user2 = User::factory()->create([
+            'name'     => 'User 2',
+            'email'    => 'user2@test.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $user1->assignRole(Role::findByName('user'));
+        $user2->assignRole(Role::findByName('user'));
+    }
+}
