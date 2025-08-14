cf42c4d8d98e06c59549cc5d28c429872875dcb6
jamiebatabyal <jamiebatabyal@gmail.com>
2025-08-14 20:39:35 +0100

    Output dto with snake case names

--- app/DataTransferObjects/Routines/RoutineData.php
+++ app/DataTransferObjects/Routines/RoutineData.php
@@ -3,8 +3,11 @@
 namespace App\DataTransferObjects\Routines;
 
 use App\Models\Routine;
+use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
+#[MapName(SnakeCaseMapper::class)]
 class RoutineData extends Data
 {
     private function __construct(
