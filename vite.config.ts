cd143b17d3c4b3969ef17b424be560a19278c539
Jamie Batabyal <jamie.batabyal@tillo.io>
2025-08-15 10:18:23 +0100

    only assign ddev vite config if ddev env url preset

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,24 @@ if (process.env.NODE_ENV === 'development') {
 
 const ddevUrl = process.env.DDEV_PRIMARY_URL ?? null
 
-const origin = ddevUrl ? `${ddevUrl.replace(/:\d+$/, "")}:5173` : null
+// Configure to work with ddev
+const server = {
+    // Respond to all network requests
+    host: "0.0.0.0",
+        port: 5173,
+        strictPort: true,
 
-export default defineConfig({
+        origin: ddevUrl ? `${ddevUrl.replace(/:\d+$/, "")}:5173` : null,
+
+    // 👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+    // Configure CORS securely for the Vite dev server to allow requests from *.ddev.site domains,
+    // supports additional hostnames (via regex). If you use another `project_tld`, adjust this.
+        cors: {
+        origin: /https?:\/\/([A-Za-z0-9\-.]+)?(\.ddev\.site)(?::\d+)?$/,
+    },
+}
+
+const config = {
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
@@ -31,24 +46,12 @@ export default defineConfig({
         }),
         vueDevtools(),
     ],
-    server: {
-        // Respond to all network requests
-        host: "0.0.0.0",
-        port: 5173,
-        strictPort: true,
-        // Defines the origin of the generated asset URLs during development, this must be set to the
-        // Vite dev server URL and selected port. In general, `process.env.DDEV_PRIMARY_URL` will give
-        // us the primary URL of the DDEV project, e.g. "https://test-vite.ddev.site". But since DDEV
-        // can be configured to use another port (via `router_https_port`), the output can also be
-        // "https://test-vite.ddev.site:1234". Therefore we need to strip a port number like ":1234"
-        // before adding Vite's port to achieve the desired output of "https://test-vite.ddev.site:5173".
-        origin,
+}
 
-        // 👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
-        // Configure CORS securely for the Vite dev server to allow requests from *.ddev.site domains,
-        // supports additional hostnames (via regex). If you use another `project_tld`, adjust this.
-        cors: {
-            origin: /https?:\/\/([A-Za-z0-9\-.]+)?(\.ddev\.site)(?::\d+)?$/,
-        },
-    },
-});
+if (ddevUrl){
+    config.server = server
+}
+
+export default defineConfig(
+    config
+);
