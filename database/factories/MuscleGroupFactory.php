2cc53a22342f22fc5a7d90a31d0136a54ed2dd5a
Jamie Batabyal <jamie.batabyal@tillo.io>
2025-08-26 15:22:46 +0100

    Improve typing on factory

--- database/factories/MuscleGroupFactory.php
+++ database/factories/MuscleGroupFactory.php
@@ -6,11 +6,14 @@ use App\Models\MuscleGroup;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
+/**
+ * @extends Factory<MuscleGroup>
+ */
 class MuscleGroupFactory extends Factory
 {
     protected $model = MuscleGroup::class;
 
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
