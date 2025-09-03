2bd2cf4220876e97627b6064777d13b5b99ad6b3
Jamie Batabyal <jamie.batabyal@tillo.io>
2025-09-03 16:09:23 +0100

    Rector run naming set (and remove)

--- rector.php
+++ rector.php
@@ -3,22 +3,27 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Exception\Configuration\InvalidConfigurationException;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
-    ])
-    // uncomment to reach your current PHP version
-     ->withPhpSets()
-    ->withTypeCoverageLevel(53)
-    ->withDeadCodeLevel(53)
-    ->withCodeQualityLevel(53)
-    ->withCodingStyleLevel(25)
-    ->withImportNames()
-    ->withComposerBased(phpunit: true,laravel: true);
+try {
+    return RectorConfig::configure()
+        ->withPaths([
+            __DIR__.'/app',
+            __DIR__.'/bootstrap',
+            __DIR__.'/config',
+            __DIR__.'/public',
+            __DIR__.'/resources',
+            __DIR__.'/routes',
+            __DIR__.'/tests',
+        ])
+        // uncomment to reach your current PHP version
+        ->withPhpSets()
+        ->withTypeCoverageLevel(53)
+        ->withDeadCodeLevel(53)
+        ->withCodeQualityLevel(53)
+        ->withCodingStyleLevel(25)
+        ->withImportNames()
+        ->withComposerBased(phpunit: true, laravel: true);
+} catch (InvalidConfigurationException $e) {
+    exit();
+}
