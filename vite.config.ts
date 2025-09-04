62b3878861ec700e2d3456f824f373b49ba663d8
jamiebatabyal <jamiebatabyal@gmail.com>
2025-09-04 09:08:49 +0100

    Remove ddev

--- vite.config.ts
+++ vite.config.ts
@@ -9,49 +9,24 @@ if (process.env.NODE_ENV === 'development') {
     vueDevtools();
 }
 
-const ddevUrl = process.env.DDEV_PRIMARY_URL ?? null
-
-// Configure to work with ddev
-const server = {
-    // Respond to all network requests
-    host: "0.0.0.0",
-        port: 5173,
-        strictPort: true,
-
-        origin: ddevUrl ? `${ddevUrl.replace(/:\d+$/, "")}:5173` : null,
-
-    // 👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
-    // Configure CORS securely for the Vite dev server to allow requests from *.ddev.site domains,
-    // supports additional hostnames (via regex). If you use another `project_tld`, adjust this.
-        cors: {
-        origin: /https?:\/\/([A-Za-z0-9\-.]+)?(\.ddev\.site)(?::\d+)?$/,
-    },
-}
-
-const config = {
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        vueDevtools(),
-    ],
-}
-
-if (ddevUrl){
-    config.server = server
-}
-
 export default defineConfig(
-    config
+    {
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            tailwindcss(),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+            vueDevtools(),
+        ],
+    }
 );
