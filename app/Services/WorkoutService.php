b6df9a9bd59fddb0695b5e9f86d943b692a23e58
Jamie Batabyal <jamie.batabyal@tillo.io>
2025-09-04 10:27:02 +0100

    Create a workout from a routine

--- app/Services/WorkoutService.php
+++ app/Services/WorkoutService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Exceptions\WorkoutServiceException;
 use App\Models\Routine;
+use App\Models\Workout;
 
 class WorkoutService
 {
@@ -12,12 +13,16 @@ class WorkoutService
     /**
      * @throws WorkoutServiceException
      */
-    public function createWorkout(Routine $routine): void
+    public function createWorkout(Routine $routine): Workout
     {
 
         if ($routine->exercises->count() === 0) {
             throw new WorkoutServiceException(self::ROUTINE_HAS_NO_EXERCISES_ERROR);
         }
+
+        return Workout::create([
+            'routine_id' => $routine->id,
+        ]);
 
     }
 }
--- tests/Tests/Feature/Services/WorkoutServiceTest.php
+++ tests/Tests/Feature/Services/WorkoutServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Services;
 
 use App\Exceptions\WorkoutServiceException;
+use App\Models\Exercise;
 use App\Models\Routine;
 use App\Services\WorkoutService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,22 +14,44 @@ class WorkoutServiceTest extends TestCase
 {
     use RefreshDatabase;
 
+    private WorkoutService $workoutService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->workoutService = new WorkoutService;
+    }
+
     #[Test]
     public function it_throw_an_exception_if_it_tries_to_create_a_workout_from_a_routine_with_no_exercises(): void
     {
         // Given
         $routine = Routine::factory()->create();
 
-        $service = new WorkoutService;
-
         // When
         try {
-            $service->createWorkout($routine);
+            $this->workoutService->createWorkout($routine);
         } catch (WorkoutServiceException $workoutServiceException) {
             // Then
             $this->assertSame('Unable to create a workout for a routine with no exercises', $workoutServiceException->getMessage());
 
         }
 
+    }
+
+    #[Test]
+    public function it_creates_a_workout_from_a_routine(): void
+    {
+        // Given
+        $routine = Routine::factory()->create();
+        $exercise = Exercise::factory()->create();
+
+        $routine->exercises()->save($exercise);
+
+        // When
+        $workout = $this->workoutService->createWorkout($routine);
+
+        // Then
+        $this->assertTrue($workout->routine->is($routine));
     }
 }
